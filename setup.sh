757c8fecf8182f442cd36a061594c605017a073e
Marko Stamenkovic <marko.stamenkovic@epfl.ch>
2017-08-08 11:30:20 +0200

    Luca's corrections

--- setup.sh
+++ setup.sh
@@ -8,25 +8,30 @@
 # Please visit: https://mstamenk.github.io/2017/07/snakemake-tutorial-for-data-analysts.html
 # for more informations
 
+# Definition of various path in the architecture of the project
+
+export MYROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+export CPP=$MYROOT/cpp
+export CPPSRC=$CPP/src
+
+export TUTO=$MYROOT
+export SCRIPTS=$MYROOT/scripts
+
+export PYTHON=$MYROOT/python
+
+export RESOURCE=$MYROOT/resource
+export ROUTINES=$MYROOT/routines
+
 # The script can be used to set the snakemake environment or not
 if [ "$1" = "snake" ]; then
     echo "Setup with snakemake using python3"
     source activate snake  
 else 
     echo "Setup without snakemake using python2"
-    source scripts/setup_path.sh
+    source $SCRIPTS/setup_path.sh
 fi
-# Definition of various path in the architecture of the project
-export CPP=$PWD/cpp
-export CPPSRC=$CPP/src
 
-export TUTO=$PWD
-export SCRIPTS=$PWD/scripts
-
-export PYTHON=$PWD/python
-
-export RESOURCE=$PWD/resource
-export ROUTINES=$PWD/routines
 
 # User-sensitive way to define your project, define the folders that are not the same from one person to the other.
 # Once the repertories updated with a git pull, every user should be able to run the full analysis
